099cb07e3173519fa5547406d69d88abc507fead
mukesh2588 <mukesh2588@gmail.com>
2018-12-01 10:44:16 +0530

    test

---
+++ src/First_Ex.java
@@ -0,0 +1,23 @@
+
+public class First_Ex {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 3; i++) {
+
+			System.out.println("Mukesh");
+		}
+		int j=0;
+		while(j<4)
+		{
+			System.out.println("MK");
+			j++;
+		}
+		int k=0;
+		do{System.out.println("mkk");
+		k++;
+		}while(k<4);
+
+
+	}
+
+}
